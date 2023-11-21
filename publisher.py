7bcae2045bee48bd512396d90cbe9c25e997cecc
João Victor <joaovfg13@gmail.com>
2023-11-20 22:25:32 -0300

    Publicar de forma automatizada dados para teste

--- publisher.py
+++ publisher.py
@@ -1,9 +1,24 @@
 from application.configs.broker_configs import mqtt_broker_configs
 import paho.mqtt.client as mqtt
+from datetime import datetime, timedelta
+import random
 
 mqtt_client = mqtt.Client('publisher-py')
 
+
+lista = []
+dia_da_semana = ["segunda", "terça", "quarta", "quinta", "sexta", "sábado", "domingo"]
+
+for dia in range(300):
+    id_equipamento = '00:00:00:00:00:00'
+    temperatura = round(random.uniform(20, 35), 1)
+    data = datetime.strptime("2023-11-18 00:00:00", "%Y-%m-%d %H:%M:%S") + timedelta(days=dia)
+    vazao_litro_acumulada = random.uniform(0, 90)
+
+    dados = f'{id_equipamento};{temperatura};{data};{dia_da_semana[random.randint(0, 6)]};{vazao_litro_acumulada}'
+    lista.append(dados)
+
 mqtt_client.connect(mqtt_broker_configs["host"], 1883)
 
-mqtt_client.publish(mqtt_broker_configs["topic"], '20.5;2022-01-01 00:00:00;segunda;12.0;1.0')
-
+for i in lista:
+    mqtt_client.publish(mqtt_broker_configs["topic"], i)
