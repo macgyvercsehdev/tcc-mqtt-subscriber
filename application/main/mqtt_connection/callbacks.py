37c3ab8645134963a95ff44c00033ed482724cf5
João Victor <joaovfg13@gmail.com>
2023-11-20 22:22:28 -0300

    Alterando o callback para melhorar a lógica de consumo

--- application/main/mqtt_connection/callbacks.py
+++ application/main/mqtt_connection/callbacks.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from application.configs.broker_configs import mqtt_broker_configs
 from application.configs.mongo import conectar_mongo
 
@@ -9,6 +9,50 @@ def enviar_para_banco(dic):
         db.mqtt.insert_one(dic)
     except Exception as e:
         print(e)
+
+def consumo_diario(dia_atual):
+    db = conectar_mongo()
+    
+    dia_anterior = datetime.strptime(dia_atual, "%Y-%m-%d %H:%M:%S") - timedelta(days=1)
+    
+    dia_anterior = datetime(dia_anterior.year, dia_anterior.month, dia_anterior.day, 0, 0, 0)
+    
+    response = list(db.mqtt.aggregate([
+        {
+            '$match': {
+                'data': {
+                    '$gte': dia_anterior, 
+                    '$lt': dia_atual
+                }
+            }
+        }, {
+            '$sort': {
+                'data': -1
+            }
+        }, {
+            '$group': {
+                '_id': {
+                    'dia': {
+                        '$dateToString': {
+                            'format': '%d-%m-%Y', 
+                            'date': '$data'
+                        }
+                    }
+                }, 
+                'vazao_litro_acumulada': {
+                    '$last': '$vazao_litro_acumulada'
+                }
+            }
+        }, {
+            '$project': {
+                '_id': 0, 
+                'dia': '$_id.dia', 
+                'vazao_litro_acumulada': 1
+            }
+        }
+    ]))
+
+    return response
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -26,15 +70,19 @@ def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
     payload = msg.payload.decode().split(";")
-    
+
     dic = {
-        "temperatura": float(payload[0]),
-        "data": datetime.strptime(payload[1], "%Y-%m-%d %H:%M:%S"),
-        "dia_da_semana": payload[2],
-        "vazao_litro": float(payload[3]),
-        "vazao_litro_acumulada": float(payload[4]),
+        "id_equipamento": payload[0],
+        "temperatura": float(payload[1]),
+        "data": datetime.strptime(payload[2], "%Y-%m-%d %H:%M:%S"),
+        "dia_da_semana": payload[3],
+        "vazao_litro_acumulada": float(payload[5]),
     }
-    
+
+    res = consumo_diario(dic["data"].strftime("%Y-%m-%d %H:%M:%S"))
+
+    dic["consumo_diario"] = res[0]["vazao_litro_acumulada"]
+
     print(dic)
     
     # TODO: Enviar o dicionário para o servidor de banco de dados
